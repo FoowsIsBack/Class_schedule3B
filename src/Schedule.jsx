178d561599a0e62099570d803fc99a9c106376bf
Foows - (Dave) <dbangcoyo11@gmail.com>
2026-06-17 21:25:54 +0800

    update schedule

--- src/Schedule.jsx
+++ src/Schedule.jsx
@@ -9,14 +9,16 @@ function Schedule() {
         <img src="video/itachi.gif" alt="Itachi"/>
         <h1>CLASS SCHEDULE</h1>
         <div className="evsu">
-          <img src="logo/logo.jpg" alt="EVSU Logo"/>
+          <a href="https://www.facebook.com/myEVSU.ormoc" target="_blank" rel="noreferrer">
+            <img src="logo/logo.jpg" alt="EVSU Logo"/>
+          </a>
         </div>
       </div>
 
       <table>
         <tbody>
           <tr>
-            <th className="subjects">📖 Subjects Enrolled for AY : 2025-2026, Sem : 2</th>
+            <th className="subjects">📖 Subjects Enrolled for AY : 2025-2026, Sem : S</th>
             <th className="status2"></th>
             <th className="status2"></th>
             <th className="status2"></th>
@@ -26,7 +28,7 @@ function Schedule() {
           </tr>
             
           <tr>
-            <th className="status">Enrollment Status: <span className="enrolled">✅ Officially Enrolled</span></th>
+            <th className="status">Enrollment Status: <span className="enrolled">✅ Processing</span></th>
             <th className="status2"></th>
             <th className="status2"></th>
             <th className="status2"></th>
@@ -45,114 +47,64 @@ function Schedule() {
             <th className="head">Professor</th>
           </tr>
 
-          <tr className="finish_sub">
-            <td>IT 323</td>
-            <td>3B</td>
-            <td>Software Engineering</td>
+          <tr className="klase">
+            <td>IT 303</td>
+            <td>3C</td>
+            <td>Information Assurance and Security 2 <br /> <strong>(Lab Schedule)</strong></td>
             <td>Monday</td>
-            <td>10:30AM - 12:00NN</td>
-            <td>IT Room 5</td>
-            <td>MORPOS, JOSEPH JAYMEL S.</td>
+            <td>9:00AM - 12:00NN</td>
+            <td>IT Room 3</td>
+            <td>PERANTE, WILFERD JUDE A.</td>
           </tr>
 
-          <tr className="finish_sub">
-            <td>IT 343</td>
-            <td>3B</td>
-            <td>Multimedia Systems <br /> <strong>(Lab Schedule)</strong></td>
+          <tr className="klase">
+            <td>IT 303</td>
+            <td>3C</td>
+            <td>Information Assurance and Security 2</td>
             <td>Monday</td>
             <td>1:00PM - 4:00PM</td>
-            <td>IT Room 3</td>
-            <td>MIRO, POL A.</td>
-          </tr>
-
-          <tr className="finish_sub">
-            <td>IT 343A</td>
-            <td>3B</td>
-            <td>IT Electives</td>
-            <td>Monday</td>
-            <td>7:30PM - 8:30PM</td>
             <td>IT Room 5</td>
-            <td>MORPOS, JOSEPH JAYMEL S.</td>
+            <td>PERANTE, WILFERD JUDE A.</td>
           </tr>
 
-          <tr className="finish_sub">
-            <td>IT 363A</td>
-            <td>3B</td>
-            <td>Application Development and Emerging Technologies</td>
+          <tr className="klase">
+            <td>IT 303</td>
+            <td>3C</td>
+            <td>Information Assurance and Security 2 <br /> <strong>(Lab Schedule)</strong></td>
             <td>Tuesday</td>
-            <td>10:00AM - 12:00NN</td>
-            <td>IT Room 4</td>
-            <td>LIGUTAN, DANIEL V.</td>
-          </tr>
-
-          <tr className="finish_sub">
-            <td>IT 383A</td>
-            <td>3B</td>
-            <td>Systems Integration & Architecture 2 <br /> <strong>(Lab Schedule)</strong></td>
-            <td>Tuesday</td>
-            <td>1:00PM - 4:00PM</td>
-            <td>IT Room 2</td>
-            <td>ASEO, MARC FRITZ Y.</td>
-          </tr>
-
-          <tr className="finish_sub">
-            <td>IT 323</td>
-            <td>3B</td>
-            <td>Software Engineering</td>
-            <td>Wednesday</td>
-            <td>10:30AM - 12:00NN</td>
-            <td>IT Room 5</td>
-            <td>MORPOS, JOSEPH JAYMEL S.</td>
-          </tr>
-
-          <tr className="finish_sub">
-            <td>CCNA 323</td>
-            <td>3B</td>
-            <td>Connecting Networks <br /> <strong>(Lab Schedule)</strong></td>
-            <td>Wednesday</td>
-            <td>1:00PM - 4:00PM</td>
-            <td>IT Room 1</td>
-            <td>BERTULFO, EDWARD B.</td>
-          </tr>
-
-          <tr className="finish_sub">
-            <td>IT 343A</td>
-            <td>3B</td>
-            <td>IT Electives</td>
-            <td>Wednesday</td>
-            <td>7:00PM - 8:30PM</td>
-            <td>IT Room 5</td>
-            <td>MORPOS, JOSEPH JAYMEL S.</td>
-          </tr>
-
-          <tr className="finish_sub">
-            <td>IT 363</td>
-            <td>3B</td>
-            <td>Information Assurance & Security 1 <br /> <strong>(Lab Schedule)</strong></td>
-            <td>Thursday</td>
-            <td>9:30AM - 12:00NN</td>
+            <td>9:00AM - 12:00NN</td>
             <td>IT Room 3</td>
-            <td>MIRO, POL A.</td>
+            <td>PERANTE, WILFERD JUDE A.</td>
           </tr>
 
-          <tr className="finish_sub">
-            <td>IT 383</td>
-            <td>3B</td>
-            <td>Integrative Programming and Technologies 2 <br /> <strong>(Lab Schedule)</strong></td>
-            <td>Thursday</td>
-            <td>1:00PM - 4:00PM</td>
-            <td>IT Room 1</td>
-            <td>ASEO, MARC FRITZ Y.</td>
-          </tr>
-
-          <tr className="finish_sub">
-            <td>IT 363A</td>
-            <td>3B</td>
-            <td>Application Development and Emerging Technologies <br /> <strong>(Lab Schedule)</strong></td>
-            <td>Thursday</td>
-            <td>5:30PM - 8:30PM</td>
+          <tr className="klase">
+            <td>IT 303</td>
+            <td>3C</td>
+            <td>Information Assurance and Security 2 <br /> <strong>(Lab Schedule)</strong></td>
+            <td>Wednesday</td>
+            <td>9:00AM - 12:00NN</td>
             <td>IT Room 3</td>
-            <td>LIGUTAN, DANIEL V.</td>
+            <td>PERANTE, WILFERD JUDE A.</td>
+          </tr>
+
+          <tr className="klase">
+            <td>IT 303</td>
+            <td>3C</td>
+            <td>Information Assurance and Security 2</td>
+            <td>Wednesday</td>
+            <td>1:00PM - 4:00PM</td>
+            <td>IT Room 5</td>
+            <td>PERANTE, WILFERD JUDE A.</td>
+          </tr>
+
+          <tr>
+            <td className="rest_day"><b>THURSDAY</b></td>
+            <td className="rest_day"><b>REST DAY</b></td>
+            <td className="rest_day"><b>REST DAY</b></td>
+            <td className="rest_day"><b>REST DAY</b></td>
+            <td className="rest_day"><b>REST DAY</b></td>
+            <td className="rest_day"><b>REST DAY</b></td>
+            <td className="rest_day"><b>REST DAY</b></td>
           </tr>
 
           <tr>
@@ -165,55 +117,14 @@ function Schedule() {
             <td className="rest_day"><b>REST DAY</b></td>
           </tr>
 
-          <tr className="finish_sub">
-            <td>IT 343</td>
-            <td>3B</td>
-            <td>Multimedia Systems</td>
-            <td>Saturday</td>
-            <td>8:00AM - 10:00AM</td>
-            <td>IT Room 3</td>
-            <td>MIRO, POL A.</td>
-          </tr>
-
-          <tr className="finish_sub">
-            <td>CCNA 323</td>
-            <td>3B</td>
-            <td>Connecting Networks</td>
-            <td>Saturday</td>
-            <td>10:00AM - 12:00NN</td>
-            <td>IT Room 4</td>
-            <td>BERTULFO, EDWARD B.</td>
-          </tr>
-
-          <tr className="finish_sub">
-            <td>IT 383</td>
-            <td>3B</td>
-            <td>Integrative Programming and Technologies 2</td>
-            <td>Saturday</td>
-            <td>1:00PM - 3:00PM</td>
-            <td>IT Room 2</td>
-            <td>ASEO, MARC FRITZ Y.</td>
-          </tr>
-
-
-          <tr className="finish_sub">
-            <td>IT 363</td>
-            <td>3B</td>
-            <td>Information Assurance and Security 1</td>
-            <td>Saturday</td>
-            <td>3:00PM - 5:00PM</td>
-            <td>IT Room 3</td>
-            <td>MIRO, POL A.</td>
-          </tr>
-
-          <tr className="finish_sub">
-            <td>IT 383A</td>
-            <td>3B</td>
-            <td>Systems Integration and Architecture 2</td>
-            <td>Saturday</td>
-            <td>5:00PM - 7:00PM</td>
-            <td>IT Room 2</td>
-            <td>ASEO, MARC FRITZ Y.</td>
+          <tr>
+            <td className="rest_day"><b>SATURDAY</b></td>
+            <td className="rest_day"><b>REST DAY</b></td>
+            <td className="rest_day"><b>REST DAY</b></td>
+            <td className="rest_day"><b>REST DAY</b></td>
+            <td className="rest_day"><b>REST DAY</b></td>
+            <td className="rest_day"><b>REST DAY</b></td>
+            <td className="rest_day"><b>REST DAY</b></td>
           </tr>
 
           <tr className="rest_day">
